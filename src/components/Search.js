0bed1356adc0bbdf6795a2abc5cc6f0fac9d7e74
Justin Li <justinlit23@gmail.com>
2022-05-13 00:45:00 -0700

    Updated Adopt functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,3 @@
-
 import React from "react"
 
 function Search({searchTerm, onChangeSearch}) {
@@ -6,19 +5,17 @@ function Search({searchTerm, onChangeSearch}) {
   function handleChange(event){
     return onChangeSearch(event.target.value)
   }
-    
 
-
-return (
+  return (
       <div className="searchbar">
-        <label htmlFor="search">Search Animals:</label>
-        <input width="500"
-          type="text"
-          id="search"
-          placeholder="Type a name to search..."
-          value={searchTerm}
-          onChange={handleChange}
-        />
+            <label htmlFor="search">Search Animals:</label>
+            <input width="500"
+              type="text"
+              id="search"
+              placeholder="Type a name to search..."
+              value={searchTerm}
+              onChange={handleChange}
+          />
       </div>
     );
   }
--- src/components/ZooAnimalCard.js
+++ src/components/ZooAnimalCard.js
@@ -1,9 +1,14 @@
 import React, {useState} from "react";
 
-function ZooAnimalCard({name, lifeSpan, habitat, diet, image}) {
+function ZooAnimalCard({name, lifeSpan, habitat, diet, image, animal, updateAdopted}) {
     const [adopt, setAdopt]=useState(false)
+    
+    
 
-    function handleAdoption(){setAdopt((adopt)=> !adopt)}
+    function handleAdoption() {
+      setAdopt(!adopt)
+      updateAdopted(animal) //removes adopted animal with DELETE, fetches another animal, and adds it to animal
+    }
 
   return ( <div className="card">
   <img className="cardPic"src={image} alt={name} width="500" height="600"/>
@@ -15,7 +20,7 @@ function ZooAnimalCard({name, lifeSpan, habitat, diet, image}) {
   </ul>
 
   {(adopt) ? (
-    <button  onClick={handleAdoption} className="adopted"> Adopted!</button>
+    <button className="adopted"> Adopted!</button>
   ) : (
     <button onClick={handleAdoption} className="primary">Adopt</button>
   )}
